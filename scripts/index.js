091c460d8f791b044570b01a15541e704210586a
Darya Cherchenko <raccoon.raccoonov@yandex.ru>
2024-05-02 13:02:09 +0300

    new review

--- scripts/index.js
+++ scripts/index.js
@@ -2,34 +2,36 @@
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const content = document.querySelector('.content');
-const cardList = content.querySelector('.places__list');
-const deleteButton = content.querySelector('.card__delete-button');
+const cardsContainer = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const addCard = (name, link) => {
+const addCard = (cardData, onDelete) => {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
   
-  cardElement.querySelector('.card__title').textContent = name;
-  cardElement.querySelector('.card__image').setAttribute('src', link);
-  cardElement.querySelector('.card__image').setAttribute('alt', name);
-  const deleteButton = cardElement.querySelector('.card__delete-button').addEventListener('click', function(){
-    deleteCard(cardElement);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => {
+    onDelete(cardElement);
   });
-  cardList.append(cardElement);
+  
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(card){
-  card.remove();
-};
+function deleteCard(delcard) {
+  delcard.remove();
+}
 
 // @todo: Вывести карточки на страницу
-function newCard(node, cards) {
+function renderInitialCards(cards) {
   cards.forEach((data) => {
-    const fun = addCard(data.name, data.link);
+    const card = addCard(data, deleteCard);
+    cardsContainer.append(card); 
   });
 }
 
-newCard(cardList, initialCards);
+renderInitialCards(initialCards);
 
 
